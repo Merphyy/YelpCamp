78e782c3a49796f05c17b8c33fc387495256c804
merphyy <619503233@qq.com>
2020-07-07 21:56:41 -0400

    second version of Yelpcamp

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var campgrounds = [
-    {name: 'Salmon Creek', image: 'https://www.lakegeorge.com/images/benchesonisland.jpg'},
-    {name: 'Granite Hill', image: 'https://www.campadk.com/campsiteguide/lincolnpond/20160531/IMG_8065.jpg'},
-    {name: 'Mountain Goats', image: 'https://www.fs.usda.gov/Internet/FSE_MEDIA/fseprd526809.jpg'}
-]
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+//connect to database
+mongoose.connect('mongodb://localhost/yelp_camp',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+//set up schema
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+//compile schema to a model
+var Campground = mongoose.model('Campground',campgroundSchema);
+
+// Campground.create({
+//     name: 'Granite Hill', 
+//     image: 'https://www.campadk.com/campsiteguide/lincolnpond/20160531/IMG_8065.jpg',
+//     description: 'This is a huge granite hill, no bathrooms. No water, beautiful granite.'
+// },(err,camp)=>{
+//     if(err){
+//         console.log('ERROR!');
+//     }else{
+//         console.log('We have added a campground');
+//         console.log(camp);
+//     }
+// });
+
+
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,25 +41,52 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds',(req,res)=> {
-    res.render('campgrounds',{campgrounds:campgrounds});
+    //get all campgrounds from db
+    Campground.find({}, (err,campgrounds)=>{
+        if(err){
+            console.log('ERROR!');
+        }else{
+            res.render('index',{campgrounds:campgrounds});
+        }
+    });
 });
 
 app.post('/campgrounds', (req,res)=>{
     //get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
+    var description = req.body.description;
     var newCampground = {
         name: name,
-        image: image
+        image: image,
+        description: description
     }
-    campgrounds.push(newCampground);
-    //redirect back to campground page
-    res.redirect('/campgrounds');
-    
+    //save to database
+    Campground.create(newCampground,(err, newlycreated)=>{
+        if(err){
+            console.log('ERROR!');
+        }else{
+            //redirect back to campground page
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
 app.get('/campgrounds/new',(req, res)=>{
     res.render('new.ejs');
+});
+
+//show shows more info about campgrounds
+app.get('/campgrounds/:id',(req,res)=>{
+    //find campground with provided id
+    Campground.findById(req.params.id,(err,found)=>{
+        if(err){
+            console.log('ERROR!');
+        }else{
+           //render show template with that campground
+            res.render('show',{campground: found});
+        }
+    });
 });
 
 app.listen(3000, () => {
--- readme.md
+++ readme.md
@@ -25,5 +25,15 @@ add a better header/title
 make campgrounds display in a grid
 
 5.style the Navbar and Form
-add a navbar to all templates
-style the new campground form
+add a navbar to all templates style the new campground form
+
+6.add mongoose
+install and configure mongoose
+setup campground model
+use campground model inside of our routes
+
+7.show page
+review the restful routes we've seen so far
+add description to our campgroundmodel
+show db.collection.drop()
+add a show route/template
